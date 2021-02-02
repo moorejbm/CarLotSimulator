7faaca2740b69785e575e4c8679b64213c73f054
Jason Moore <moorejbm@gmail.com>
2021-02-02 10:21:28 -0600

    Update3

--- CarLotSimulatorApp/CarLotSimulator/Car.cs
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace CarLotSimulator
 {
-    class Car
+    public class Car
     {
-    }
+        public Car()
+        {
+        }
+
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDrivable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            honkNoise = honkNoise;
+            IsDrivable = isDrivable;
+    
+         //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+         public int Year { get; set; }
+         public string Make { get; set; }
+         public string Model { get; set; }
+         public string EngineNoise { get; set; }
+         public bool IsDrivable { get; set; }
+
+
+
+}
 }
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -13,6 +13,23 @@ namespace CarLotSimulator
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
 
+            CarLot carvana = new CarLot();
+                        
+            var car1 = new Car(2014, "Dodge", "Challenger", "Hummm", "Beep", true );
+            var car2 = new Car(2003, "Jeep", "Wrangler", "Vroom", "Beep Beep", true);
+            var car3 = new Car();
+            {
+                    Year = 2018,
+                    Make = "Honda",
+                    Model ="Accord",
+                    EngineNoise = "Vroom",
+                    HonkNoise = "Beep",
+                    IsDrivable = true,
+            };
+            carvana.Cars.Add(car1);
+            carvana.Cars.Add(car2);
+            carvana.Cars.Add(car3);
+
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
@@ -28,6 +45,21 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
-        }
+
+            foreach(var car in carvana.Car)
+            {
+                Console.WriteLine(".............................");
+                Console.WriteLine(car.Make);
+                Console.WriteLine(car.Model);
+                Console.WriteLine(car.Year);
+                Console.WriteLine(car.EngineNoise);
+                Console.WriteLine(car.HonkNoise);
+
+                if(car.IsDrivable)
+                {
+                    Console.WriteLine("This car can drive");
+                }
+
+            }
     }
 }
